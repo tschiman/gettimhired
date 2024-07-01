4eb9b09ec30d9d728e06dd2a157c0c83cbb26fba
Tim Schimandle <tschiman@gmail.com>
2024-07-01 15:28:13 -0600

    First Commit

---
+++ src/main/java/com/gettimhired/ResumeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.gettimhired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeSiteApplication.class, args);
+	}
+
+}
