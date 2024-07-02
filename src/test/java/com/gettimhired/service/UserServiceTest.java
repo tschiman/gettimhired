40ee174f105e7d075e24fdc1439e923c42ffe055
Tim Schimandle <tschiman@gmail.com>
2024-07-02 13:23:07 -0600

    Add user service test

---
+++ src/test/java/com/gettimhired/service/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.gettimhired.service;
+
+import com.gettimhired.model.mongo.User;
+import com.gettimhired.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+
+class UserServiceTest {
+
+    private UserService userService;
+
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void init() {
+        userRepository = Mockito.mock(UserRepository.class);
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void testCreateUserHappy() {
+        Mockito.when(userRepository.save(any())).thenReturn(new User("BARK_USER", "BARK_PASSWORD"));
+
+        User user = userService.createUser();
+
+        assertNotNull(user.id());
+        assertNotNull(user.password());
+        Mockito.verify(userRepository, times(1)).save(any(User.class));
+    }
+
+}
