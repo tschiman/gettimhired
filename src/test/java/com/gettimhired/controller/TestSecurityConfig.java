8a63c0fdb446f0acaa58e785336a17dd312a4130
Tim Schimandle <tschiman@gmail.com>
2024-07-08 15:59:16 -0600

    Add Candidate API integration tests

---
+++ src/test/java/com/gettimhired/controller/TestSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.gettimhired.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class TestSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChainLocal(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(basic -> basic.init(http))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/api/**").authenticated();
+                    authorize.requestMatchers("/graphql").authenticated();
+                    authorize.anyRequest().permitAll();
+                })
+                .userDetailsService(userDetailsService())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user1")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
